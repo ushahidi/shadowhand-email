57b858a94cca541721e2cc0e8d2af46f8e6b5bef
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-11 23:39:05 +0800

    Delete init.php

--- init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Swift::init(function () {
-    $charset = Kohana::$charset;
-
-    // Set the default character set for everything
-    Swift_Preferences::getInstance()->setCharset($charset);
-});
